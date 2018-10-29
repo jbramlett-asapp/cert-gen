ee8f48413b53398a1858b55b35d3d1e75820029c
John Bramlett <jbramlett@asapp.com>
2018-10-29 10:31:45 -0400

    Updating

--- make_ca.sh
+++ make_ca.sh
@@ -1,4 +1,22 @@
+#!/usr/bin/env bash
 
 openssl genrsa -out rootCA.key 4096
 
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt -subj "/C=US/ST=New York/L=New York/O=ASAPP/OU=Platform/CN=asapp.local"
+if [ -z "$COMMON_NAME" ];
+then
+	COMMON_NAME=$HOSTNAME
+fi
+
+if [ -z "$ORGANIZATION" ]; 
+then
+	ORGANIZATION="ASAPP"
+fi
+
+if [ -z "$ORGANIZATION_UNIT" ]; 
+then
+	ORGANIZATION_UNIT="PLATFORM"
+fi
+
+SUBJECT="/C=US/ST=NY/L=New York/O=$ORGANIZATION/OU=$ORGANIZATION_UNIT/CN=$COMMON_NAME"
+
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt -subj "$SUBJECT"
--- make_cert.sh
+++ make_cert.sh
@@ -1,16 +1,53 @@
+#!/usr/bin/env bash
+
+if [ -z "$COMMON_NAME" ];
+then
+	COMMON_NAME=$HOSTNAME
+fi
+
+if [ -z "$ORGANIZATION" ]; 
+then
+	ORGANIZATION=ASAPP
+fi
+
+if [ -z "$ORGANIZATION_UNIT" ]; 
+then
+	ORGANIZATION=PLATFORM
+fi
+
+if [ -z "$SAN" ];
+then
+	SAN=*DNS:*.local
+fi
+
+
+USAGE="Usage: $0 root_ca.crt-file root_ca.key-file output_file_root"
+
+if [ "$#" -lt "3" ]
+then
+  echo "$USAGE"
+  exit 1
+fi
+
+CA_CERT=$1
+CA_KEY=$2
+OUTPUT_FILE_ROOT=$3
+
+SUBJECT="/C=US/ST=New York/L=New York/O=$ORGANIZATION/OU=$ORGANIZATION_UNIT/CN=$COMMON_NAME"
+
 openssl req -new -sha256 \
-    -key mydomain.com.key \
-    -subj "/C=US/ST=NY/O=ASAPP, Inc./CN=mytestdomain.com" \
+    -key $OUTPUT_FILE_ROOT.key \
+    -subj "$SUBJECT" \
     -reqexts SAN \
     -config <(cat /etc/ssl/openssl.cnf \
-        <(printf "\n[SAN]\nsubjectAltName=DNS:*.local,DNS:*.dc")) \
-    -out mydomain.com.csr
+        <(printf "\n[SAN]\nsubjectAltName=$SAN")) \
+    -out $OUTPUT_FILE_ROOT.csr
 
 
 # openssl req -new -config cert.conf -subj "CN=mytestdomain.com" -keyout mydomain.com.key -out mydomain.com.csr
 
-openssl req -in mydomain.com.csr -noout -text
+#openssl req -in $OUTPUT_FILE_ROOT.csr -noout -text
 
-openssl x509 -req -in mydomain.com.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out mydomain.com.crt -days 500 -sha256
+openssl x509 -req -in $OUTPUT_FILE_ROOT.csr -CA $CA_CERT -CAkey $CA_KEY -CAcreateserial -out $OUTPUT_FILE_ROOT.pem -days 3650 -sha256
 
-openssl x509 -in mydomain.com.crt -text -noout
+#openssl x509 -in mydomain.com.crt -text -noout
